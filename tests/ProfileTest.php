72abf3d831f50a01f34aba4cce8e79b81784e17b
Salvatore Santagati <me@salsan.dev>
2024-04-29 18:41:11 +0200

    chore: added unit test for GetTournaments function

--- tests/ProfileTest.php
+++ tests/ProfileTest.php
@@ -32,7 +32,7 @@ final class ProfileTest extends TestCase
         $this->assertStringContainsString('-', $player["eloOnlineBullet"]);
         $this->assertStringContainsString('-', $player["eloOnlineBlitz"]);
         $this->assertStringContainsString('-', $player["eloOnlineRapid"]);
-        $this->assertStringContainsString('165714' , $player["fsiId"]);
+        $this->assertStringContainsString('165714', $player["fsiId"]);
         $this->assertStringContainsString('2832408', $player["fideId"]);
         $this->assertMatchesRegularExpression('/^\d{2}-\d{2}-\d{4}$/', $player["lastTournament"]);
         $this->assertStringContainsString('1982', $player["yearBirthday"]);
@@ -50,5 +50,41 @@ final class ProfileTest extends TestCase
     {
         $total = $profile->getNumberTournaments();
         $this->assertLessThanOrEqual(19, $total);
+    }
+
+    /**
+     * @depends testInit
+     */
+    public function testGetTournaments($profile): void
+    {
+        $tournaments = $profile->getTournaments();
+        $this->assertIsArray($tournaments);
+        $this->assertStringContainsStringIgnoringCase('FESTIVAL MILAZZO ISOLE EOLIE - B', $tournaments["2209005B"]["name"]);
+        $this->assertStringContainsString('CT', $tournaments["1501012A"]["province"]);
+        $this->assertStringContainsString('02-01-2015', $tournaments["1501012A"]["startData"]);
+        $this->assertStringContainsString('04-01-2015', $tournaments["1501012A"]["endData"]);
+        $this->assertStringContainsString('0', $tournaments["2209005B"]["eloVariation"]);
+        $this->assertStringContainsString('1419' , $tournaments["2209005B"]["averageOpponent"]);
+        $this->assertStringContainsString('7' , $tournaments["2209005B"]["numberOfMatchFide"]);
+        $this->assertStringContainsString('2.5' , $tournaments["2209005B"]["fidePoint"]);
+        $this->assertStringContainsString('1621' , $tournaments["2403032D"]["performance"]);
+        $this->assertStringContainsString('5' , $tournaments["2403032D"]["totalNumberOfRounds"]);
+        $this->assertStringContainsString('279' , $tournaments["2403032D"]["totalPlayers"]);
+        $this->assertStringContainsString('98' , $tournaments["2403032D"]["rankingPosition"]);
+        $this->assertStringContainsString('2.5' , $tournaments["2403032D"]["points"]);
+        $this->assertStringContainsString('5', $tournaments["2403032D"]["games"]);
+        $this->assertStringContainsString('2', $tournaments["2403032D"]["gamesWin"]);
+        $this->assertStringContainsString('1', $tournaments["2403032D"]["gamesDraw"]);
+        $this->assertStringContainsString('2', $tournaments["2403032D"]["gamesLoss"]);
+        $this->assertStringContainsString('2', $tournaments["2403032D"]["gamesWinWhite"]);
+        $this->assertStringContainsString('0', $tournaments["2403032D"]["gamesDrawWhite"]);
+        $this->assertStringContainsString('0', $tournaments["2403032D"]["gamesLossWhite"]);
+        $this->assertStringContainsString('0', $tournaments["2403032D"]["gamesWinBlack"]);
+        $this->assertStringContainsString('1', $tournaments["2403032D"]["gamesDrawBlack"]);
+        $this->assertStringContainsString('2', $tournaments["2403032D"]["gamesLossBlack"]);
+        $this->assertStringContainsString('0', $tournaments["2403032D"]["gamesWinsForfeit"]);
+        $this->assertStringContainsString('0', $tournaments["2403032D"]["gamesLossForfeit"]);
+        $this->assertStringContainsString('2', $tournaments["2403032D"]["gamesWhite"]);
+        $this->assertStringContainsString('3', $tournaments["2403032D"]["gamesBlack"]);
     }
 }
